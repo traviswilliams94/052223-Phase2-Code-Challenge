09efc0f52d8f8855356f307c14958b540bdabc04
Travis <travis.williams94@gmail.com>
2023-06-22 10:54:21 -0600

    added sort functionality

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -6,7 +6,17 @@ import AddTransactionForm from "./AddTransactionForm";
 function AccountContainer({ transactions, setTransactions}) {
 
 const [searchState, setSearchState] = useState("");
-const searchedTransactions = transactions.filter(transaction => transaction.description.toLocaleLowerCase().includes(searchState.toLocaleLowerCase()))
+const [sort, setSort] = useState("Category");
+const sortedItems = [...transactions].sort((transaction1, transaction2) => {
+  if (sort === "Category") {
+    return transaction1.category.localeCompare(transaction2.category);
+  } else {
+    return transaction1.description.localeCompare(transaction2.description);
+  }
+});
+
+const searchedTransactions = sortedItems.filter(transaction => transaction.description.toLocaleLowerCase().includes(searchState.toLocaleLowerCase()))
+
 
 function handleSearch(e) {
   setSearchState(e.target.value)
@@ -22,9 +32,11 @@ function deleteTransaction(id) {
   setTransactions(updatedTransactions)
 }
 
+
+
   return (
     <div>
-      <Search searchState={searchState} handleSearch={handleSearch}/>
+      <Search searchState={searchState} handleSearch={handleSearch} sort={sort} onChangeSort={setSort}/>
       <AddTransactionForm addTransaction={addTransaction}/>
       <TransactionsList transactions={transactions} searchedTransactions={searchedTransactions} deleteTransaction={deleteTransaction}/>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,20 @@
 import React from "react";
 
-function Search({searchState, handleSearch}) {
+function Search({searchState, handleSearch, onChangeSort, sort}) {
+
+function handleSortChange(e){
+  onChangeSort(e.target.value)
+}
+
   return (
     <div className="ui large fluid icon input">
+      <label>
+        <strong>Sort By: </strong>
+        <select onChange={handleSortChange} value={sort}>
+          <option value="Category">Category</option>
+          <option value="Description">Description</option>
+        </select>
+      </label>
       <input
         type="text"
         placeholder="Search your Recent Transactions"
